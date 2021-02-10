583cbb223b3be9af174deb576f93514dc5b3dcb7
floup42 <pousseurdec@gmail.com>
2021-02-11 00:59:03 +0100

    login_v01_ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @user=User.all
   end
 
-  before_action :set_user, only: %i[:show, :edit, :update, :destroy]
   def show
   end
 
   def edit
-  end
-
-  def update
   end
 
   def destroy
